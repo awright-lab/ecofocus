6c261f2fdd1e71a2f36bf64cdfd798edfd0d0925
arif-wright <arif.wright@gmail.com>
2025-07-30 15:20:20 -0500

    changing homepage layout v32

--- app/home/CoreServices.tsx
+++ app/home/CoreServices.tsx
@@ -40,7 +40,7 @@ export default function CoreServices() {
             </div>
 
             {/* Accent Bar */}
-            <div className="absolute -top-20 right-0 w-[150%] h-64 bg-gradient-to-l from-emerald-500 to-blue-500 opacity-30 rotate-[-8deg]"></div>
+            <div className="absolute top-[-40px] right-[-20%] w-[150%] h-32 bg-gradient-to-l from-emerald-500 to-blue-500 opacity-30 rounded-full rotate-[-8deg]"></div>
 
             <div className="relative z-10 max-w-6xl mx-auto px-6">
                 {/* Category Tag */}
