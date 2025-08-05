5b10db328b03e6c915d4779a228143f3d74ac418
arif-wright <arif.wright@gmail.com>
2025-08-04 21:39:42 -0500

    Update Hero component to CMS-driven content with next/image optimization v3

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-'use client';
-
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import StickyButtons from '../components/StickyButtons';
 
 import Hero from './home/Hero';
-import QuickStats from './home/QuickStats';
-import FeaturedReport from './home/FeaturedReport';
+//import QuickStats from './home/QuickStats';
+//import FeaturedReport from './home/FeaturedReport';
 import FeaturedDashboard from './home/FeaturedDashboard';
 import CoreServices from './home/CoreServices';
-import WhyChoose from './home/WhyChoose';
+//import WhyChoose from './home/WhyChoose';
 import TrustedBy from './home/TrustedBy';
 import CallToAction from './home/CallToAction';
-import Newsletter from './home/Newsletter';
+//import Newsletter from './home/Newsletter';
 import EcoNuggetInsights from './home/EcoNuggetInsights';
 
 export default function HomePage() {
@@ -30,6 +28,7 @@ export default function HomePage() {
     </main>
   );
 }
+
 
 
 
