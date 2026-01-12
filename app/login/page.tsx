69a165b5ee0e6d850b07e0f3c014ab89fb257df1
arif-wright <arif.wright@gmail.com>
2026-01-12 14:20:15 -0600

    developing EcoFocus Displayr Lite

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 import LoginForm from "./LoginForm";
 
-export default function LoginPage({ searchParams }: { searchParams: { redirect?: string } }) {
-  const redirect = searchParams?.redirect || "/portal";
+export default function LoginPage(props: { searchParams?: Record<string, string | string[] | undefined> }) {
+  const redirectParam = props.searchParams?.redirect;
+  const redirect = Array.isArray(redirectParam) ? redirectParam[0] : redirectParam || "/portal";
   return <LoginForm redirect={redirect} />;
 }
